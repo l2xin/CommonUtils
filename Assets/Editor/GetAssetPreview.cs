a73a6d17123bc8cc8d430cc2a8e193d21857bf6c
RoundLogs <407173079@qq.com>
2019-03-28 09:45:56 +0800

    GetAssetsPreview

---
+++ Assets/Editor/GetAssetPreview.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GetAssetPreview : MonoBehaviour {
+
+    [MenuItem("Assets/PreviewPrefab %#R")]
+    public static void PreviewPrefab()
+    {
+        UnityEngine.Object[] arr = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.DeepAssets);
+        if(arr == null)
+        {
+            return;
+        }
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] is GameObject)
+            {
+                GetAssetPreview.SaveOnePreview(arr[i] as GameObject, AssetDatabase.GetAssetPath(arr[i]));
+            }
+        }
+        AssetDatabase.Refresh();
+    }
+
+    private static void SaveOnePreview(GameObject go, string filePath)
+    {
+        var texture2d = AssetPreview.GetAssetPreview(go);
+        if(texture2d == null)
+        {
+            return;
+        }
+        byte[] bytes = texture2d.EncodeToPNG();
+        if (bytes == null)
+        {
+            return;
+        }
+        string xx = filePath.Replace(".prefab", ".png").Replace("Assets", "");
+        string filename = Application.dataPath + xx;
+        Debug.Log("filename:" + filename);
+        System.IO.File.WriteAllBytes(filename, bytes);
+        bytes = null;
+        texture2d = null;
+
+    }
+   
+
+}
